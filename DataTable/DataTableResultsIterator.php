f51e0c22dd27ae5f10bf5dd3485fa2a77b8af02c
Rafael Nájera <rafael@najera.ca>
2024-01-30 11:52:42 +0100

    Added Countable to DataTableResultsIterator

--- DataTable/DataTableResultsIterator.php
+++ DataTable/DataTableResultsIterator.php
@@ -1,7 +1,30 @@
 <?php
-
+/*
+ * The MIT License
+ *
+ * Copyright 2017-24 Thomas-Institut, Universität zu Köln.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 namespace ThomasInstitut\DataTable;
 
+use Countable;
 use Iterator;
 
 /**
@@ -12,6 +35,7 @@ use Iterator;
  * methods:
  *
  * * count(): returns the number of results without traversing the result set
+ *   (implements the Countable interface, so count($iterator) works)
  * * getFirst(): returns the first result or null if there are no results
  *
  * The iterator strictly returns only arrays as results and iterator keys
@@ -23,7 +47,7 @@ use Iterator;
  * for example, MySql, cannot be rewound.
  *
  */
-interface DataTableResultsIterator extends Iterator
+interface DataTableResultsIterator extends Iterator, Countable
 {
 
     /**
